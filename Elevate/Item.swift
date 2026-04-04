98bf4037bb9b9ffa935c4788e75c30c68abe6848
Tomer Abramovich <tomerabr@gmail.com>
2026-04-04 12:28:19 +0300

    Initial Commit

---
+++ Elevate/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Elevate
+//
+//  Created by Tomer Abramovich on 04/04/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ElevateTests/ElevateTests.swift
@@ -0,0 +1,17 @@
+//
+//  ElevateTests.swift
+//  ElevateTests
+//
+//  Created by Tomer Abramovich on 04/04/2026.
+//
+
+import Testing
+@testable import Elevate
+
+struct ElevateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
